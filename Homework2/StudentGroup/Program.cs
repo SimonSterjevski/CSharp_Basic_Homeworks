c915fb2426cd5eb7855bfeff7a07f5cb773cf5ea
unknown <ssterjevski@yahoo.com>
2020-03-12 14:02:24 +0100

    Homework numner 2 added :)

---
+++ Homework2/StudentGroup/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace StudentGroup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] studentsG1 = { "Mark", "Joe", "Rudy", "Bob", "Bam" };
+            string[] studentsG2 = { "Damjan", "Jovan", "Robert", "Petar", "Branko" };
+            Console.WriteLine("Please enter the student group (1 or 2)");
+            string groupNumber = Console.ReadLine();
+
+            if (groupNumber == "1")
+            {
+                Console.WriteLine("Students in group 1 are:");
+                for (int i = 0; i < studentsG1.Length; i++)
+                {
+                    Console.WriteLine(studentsG1[i]);
+                }
+            } else if (groupNumber == "2")
+            {
+                Console.WriteLine("Students in group 2 are:");
+                for (int j = 0; j < studentsG2.Length; j++)
+                {
+                    Console.WriteLine(studentsG2[j]);
+                }
+            } else
+                {
+                    Console.WriteLine("There are no student in the group you entered");
+                }
+            Console.ReadLine();
+        }
+        }
+    }
+
---
+++ Homework2/SumOfEven/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SumOfEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter 6 integer numbers");
+            int[] firstArray = new int[6];
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                bool checkIfInt = int.TryParse(Console.ReadLine(), out int parsedNum);
+                if (checkIfInt)
+                {
+                    firstArray[i] = parsedNum;
+                }
+                else
+                {
+                    Console.WriteLine("You entered wrong number. Please enter valid number");
+                    i--;
+                }
+            }
+
+            int sum = 0;
+
+            for (int j = 0; j < firstArray.Length; j++)
+            {
+                if (firstArray[j] % 2 == 0)
+                {
+                    sum += firstArray[j];
+                }
+            }
+            Console.WriteLine("The sum of all even numbers is " + sum);
+            Console.ReadLine();
+        }
+    }
+}
