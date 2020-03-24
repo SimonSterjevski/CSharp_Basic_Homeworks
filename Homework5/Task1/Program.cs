f0d24094fa71caa7cffbf8d53c1d736b23df769e
unknown <ssterjevski@yahoo.com>
2020-03-24 14:22:44 +0100

    Homework 5 added :)

---
+++ Homework5/Task1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+
+        static void Substrings(string greeting)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Please enter a number");
+                bool ifValid = int.TryParse(Console.ReadLine(), out int n);
+                if (ifValid)
+                {
+                    if (n > 0 && n <= greeting.Length)
+                    {
+                        Console.Clear();
+                        string newString = greeting.Substring(0, n);
+                        Console.WriteLine(newString);
+                        Console.WriteLine($@"The new string has {newString.Length} characters");
+                        Console.ReadLine();
+                        break;
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine($@"Error! Please press enter and insert number bigger than 0 and smaller than {greeting.Length + 1}");
+                        Console.ReadLine();
+                    }
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Error! Please press enter and insert a number");
+                    Console.ReadLine();
+                }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Substrings("Hello from SEDC Codecademy v7.0");
+          
+        }
+    }
+}
---
+++ Homework5/Task2/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+
+        static string Sum(int num1, int num2)
+        {
+            return $@"The result is {num1 + num2}";
+        }
+
+        static string Subtract(int num1, int num2)
+        {
+            return $@"The result is {num1 - num2}";
+        }
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Hi! This is a simple calculator. Please enter operator (+ or -)");
+                string operation = Console.ReadLine();
+                if (operation == "+" || operation == "-")
+                {
+                    Console.Clear();
+                    Console.WriteLine("Now please insert two numbers");
+                    while (true)
+                    {
+                        Console.Write("First number is: ");
+                        bool ifValidFirst = int.TryParse(Console.ReadLine(), out int firstNumber);
+                        if (ifValidFirst)
+                        {
+                            while (true)
+                            {
+                                Console.Clear();
+                                Console.Write("Second number is: ");
+                                bool ifValidSecond = int.TryParse(Console.ReadLine(), out int secondNumber);
+                                if (ifValidSecond)
+                                {
+                                    if (operation == "+")
+                                    {
+                                        Console.Clear();
+                                        Console.WriteLine(Sum(firstNumber, secondNumber));
+                                        Console.ReadLine();
+                                        
+                                    }
+                                    else
+                                    {
+                                        Console.Clear();
+                                        Console.WriteLine(Subtract(firstNumber, secondNumber));
+                                        Console.ReadLine();
+                                        
+                                    }
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Error! Please press enter and insert valid number");
+                                    Console.ReadLine();
+                                    Console.Clear();
+                                }
+                            }
+                            break;
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Error! Please press enter and insert valid number");
+                            Console.ReadLine();
+                            Console.Clear();
+                        }
+                    }
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Error! Please press enter and insert valid operator");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
