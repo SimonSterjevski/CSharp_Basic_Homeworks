dfc4489dde1d3f81017bc2276ab4a2f6f86b0e2a
unknown <ssterjevski@yahoo.com>
2020-04-26 17:46:01 +0200

    Homework_MovieStore added :)

---
+++ Homework_MovieStore/Classes/Class/Employee.cs
@@ -0,0 +1,87 @@
+﻿using Classes.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes.Class
+{
+    public class Employee : Member
+    {
+        private int Salary { get; set; }
+        public int HoursPerMonth { get; set; }
+        private int? Bonus { get; set; }
+        public void SetBonus()
+        {
+            if (HoursPerMonth > 160)
+            {
+                Bonus = 30 / 100;
+            }
+            else
+            {
+                Bonus = null;
+            }
+        }
+        public void SetSalary()
+        {
+            if (Bonus.HasValue)
+            {
+                Salary += HoursPerMonth * Bonus.Value;
+            }
+            else
+            {
+                Salary = Salary;
+            }
+
+        }
+        public Employee(string name, string surname, string username, string pass) : base(Roles.Employee)
+        {
+            FirstName = name;
+            LastName = surname;
+            UserName = username;
+            Password = pass;
+            Role = Roles.Employee;
+
+        }
+
+        public void AddMember(User user, List<User> members)
+        {
+            DateTime now = DateTime.Today;
+            members.Add(user);
+            user.DateOfRegistration = now;
+        }
+
+        public void DeleteMember(User user, List<User> members)
+        {
+            foreach (Movie usermovies in user.Movies)
+            {
+                user.Movies.Remove(usermovies);
+            }
+            members.Remove(user);
+        }
+
+        public void SeeMembers(List<User> members)
+        {
+            Console.WriteLine("List od registered members:");
+            foreach (User user in members)
+            {
+                Console.WriteLine($"{user.FirstName} {user.LastName} ID number: {user.MemberNumber}");
+            }
+
+        }
+
+        public void SeeAvaliableMovies(Movie[] all, List<Movie> rented)
+        {
+            Console.WriteLine("List od avaliable movies:");
+            foreach (Movie film in all)
+            {
+                int index = rented.IndexOf(film);
+                if (index == -1)
+                {
+                    Console.WriteLine(film.Title);
+                }
+            }
+
+        }
+
+    }
+}
---
+++ Homework_MovieStore/MovieApp/Program.cs
@@ -0,0 +1,51 @@
+﻿using Classes.Class;
+using Classes.Enum;
+using System;
+using System.Collections.Generic;
+
+namespace MovieApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie gladiator = new Movie("Gladiator", "Epic historical action drama", 2000, MovieGenre.Historical);
+            Movie theBestOffer = new Movie("The Best Offer", "Italian psychological thriller", 2013, MovieGenre.Thriller);
+            Movie theNakedGun = new Movie("The Naked Gun", "Crime comedy film", 1988, MovieGenre.Comedy);
+            Movie theSunsetLimited = new Movie("The Sunset Limited", "Philosophical drama", 2011, MovieGenre.Drama);
+            Movie braveheart = new Movie("Braveheart", "Epic war drama", 1995, MovieGenre.Historical);
+            Movie interstellar = new Movie("Interstellar", "Epic sciense fiction", 2014, MovieGenre.ScienceFiction);
+            Movie[] allMovies = { gladiator, theBestOffer, theNakedGun, theSunsetLimited, braveheart, interstellar };
+            List<Movie> rentedMovies = new List<Movie> { };
+            List<User> registeredMembers = new List<User> { };
+            User user1 = new User(1, SubscriptionType.GoldenMember, new List<Movie> { }, "Toni", "Kukoc", "toni", "tk");
+            User user2 = new User(2, SubscriptionType.Monthly, new List<Movie> { }, "Dino", "Radza", "dino", "dr");
+            User user3 = new User(3, SubscriptionType.Annually, new List<Movie> { }, "Arijan", "Komazec", "arijan", "ak");
+            Employee employee1 = new Employee("Goran", "Goranov", "goran", "gg");
+
+            employee1.AddMember(user1, registeredMembers);
+            employee1.AddMember(user2, registeredMembers);
+            employee1.AddMember(user3, registeredMembers);
+            //employee1.DeleteMember(user2, registeredMembers);
+            employee1.SeeMembers(registeredMembers);
+            Console.WriteLine("----------------");
+            user2.Login(user2, registeredMembers);
+            Console.WriteLine("----------------");
+            user3.Login(user3, registeredMembers);
+            user2.RentMovie(braveheart, user2, rentedMovies, registeredMembers);
+            user3.RentMovie(gladiator, user3, rentedMovies, registeredMembers);
+            user3.RentMovie(theNakedGun, user3, rentedMovies, registeredMembers);
+            Console.WriteLine("----------------");
+            user3.RentMovie(braveheart, user3, rentedMovies, registeredMembers);
+            Console.WriteLine("----------------");
+            employee1.SeeAvaliableMovies(allMovies, rentedMovies);
+            Console.WriteLine("----------------");
+            user3.SeeInfo(user3, registeredMembers);
+            Console.WriteLine("----------------");
+            user3.SeeRented(user3, registeredMembers);
+            Console.WriteLine("----------------");
+            user1.SeeRented(user1, registeredMembers);
+            Console.ReadLine();
+        }
+    }
+}
